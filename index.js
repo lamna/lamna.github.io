36954263671c3d7ecd25a715dc2d573b05611567
lamna <lemnasachin@gmail.com>
2016-05-05 22:41:04 -0700

    landing page 5-5-2016

--- index.js
+++ index.js
@@ -11,5 +11,24 @@ $(document).ready(function(){
 	$( "#contact" ).click(function() {
 		$( "#email_phone" ).slideToggle( "slow" );		
     });
+	
+	
+	
+	(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
 		
 });
