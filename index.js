1d8410acfde04d813aac2e60fad6e27c5f787c86
lamna <lemnasachin@gmail.com>
2016-07-18 22:35:10 -0700

    updated my portfolio

--- index.js
+++ index.js
@@ -1,18 +1,82 @@
+
 $(document).ready(function(){
-	$('#about').click(function(){
-		$('#aboutme').slideToggle( "slow" );		
-	});
-	
-	
-	$('#projects').click(function(){
-		$('#websites').slideToggle( "slow" );		
-	});
-	
-	$( "#contact" ).click(function() {
-		$( "#email_phone" ).slideToggle( "slow" );		
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
     });
+  });
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+$(window).load(function() {
+	$(".loader").fadeOut(5000);
 	
-	
+});
+
+
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+
+
+$(document).on("scroll", function(){
+	if
+     ($(document).scrollTop() > 100){
+	  $(".myLogo").addClass("shrinkLogo");		
+	}
+	else{
+		$(".myLogo").removeClass("shrinkLogo");}
+});
+
 	
 	(function() {
 
@@ -30,5 +94,7 @@ $(document).ready(function(){
     showNextQuote();
     
 })();
-		
-});
+
+
+ });
+
