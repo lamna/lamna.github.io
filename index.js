f5cd7d6334791edc79f6e1ce5dc0e8f70856a585
lamna <lemnasachin@gmail.com>
2017-03-17 21:18:55 -0700

    updated landing page
    updated landing page on 3-17-17

--- index.js
+++ index.js
@@ -4,26 +4,20 @@ function myFunction(x) {
 }
 
 $(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
+  
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 900, function(){
    
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
   
   $(window).scroll(function() {
@@ -98,5 +92,69 @@ $(document).on("scroll", function(){
 })();
 
 
+
+
+// =============================================
+// animation on scroll
+// =============================================
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+		$element.addClass('animated fadeInDown');
+
+    } else {
+	 $element.removeClass('animated fadeInDown');
+
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+// =============================================
+// Image modal
+// =============================================
+
+
+
+
+// =============================================
+// wow js --animation delay
+// =============================================
+
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
+
+
+//-------------------------------jquey closing tab
  });
 
